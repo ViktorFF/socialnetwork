0d5b4ffed0804b0aa84aed0d0efb05bd7e5e5c31
ViktorFF <victor.bahanovich@gmail.com>
2019-12-17 19:54:56 +0300

    first commit

---
+++ src/main/java/by/mysocialnetwork/entity/User.java
@@ -0,0 +1,11 @@
+package by.mysocialnetwork.entity;
+
+public class User {
+    private static int icrId = 1;
+    private int id = icrId++;
+    private String name;
+    private String lastName;
+    private int age;
+    private String login;
+    private String password;
+}
